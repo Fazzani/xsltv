67d022a4fccd164762e3c57072de1d38655372a0
Fazzani <tunisienheni@outlook.com>
2018-10-02 23:07:39 +0200

    fix

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 var express = require("express");
-var path    = require("path");
-var app     = express();
+var path = require("path");
+var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 //Store all JS and CSS in Scripts folder.
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/public/index.html'));
-  //__dirname : It will resolve to your project folder.
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    //__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000);
+app.listen(PORT);
 
-console.log("Running at Port 3000");
+console.log("Running at Port " + PORT);
