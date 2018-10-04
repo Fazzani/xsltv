40add51c7318a26451f105e84eec2813086a4458
Fazzani <tunisienheni@outlook.com>
2018-10-04 17:03:38 +0200

    fix timeline

--- public/js/init.js
+++ public/js/init.js
@@ -206,7 +206,7 @@ $(function () {
             });
         });
 
-        InitTimeline($tvFrame, $vline);
+        InitTimeline($tvFrame, $vline, hours);
 
         $tvFrame.show();
     }
@@ -221,7 +221,7 @@ $(function () {
 });
 
 
-function InitTimeline($tvFrame, $vline) {
+function InitTimeline($tvFrame, $vline, hours) {
     let startTimeInit = $tvFrame.find('th.time[onclick^=Init]').attr('onclick');
     let startTime = (startTimeInit.split('Init(')[1].split(')')[0]).split(',');
     let startTimeMoment = `${startTime[2]}-${startTime[3]}-${startTime[4]} ${startTime[1]}:00:00`;
@@ -231,24 +231,23 @@ function InitTimeline($tvFrame, $vline) {
         if (undefined == paddingLeft) {
             paddingLeft = ($('div.leftchannel').width() / $tvFrame.width()) * 100;
         }
-        $vline.css('margin-left', (percentElapsedTimeNowByDay(startTimeMoment) + paddingLeft) + '%');
+        $vline.css('margin-left', (percentElapsedTimeNowByDay(startTimeMoment, hours) + paddingLeft) + '%');
     }, 1000);
 }
 
-function percentElapsedTimeNowByDay(startDay) {
+function percentElapsedTimeNowByDay(startDay, hours) {
     const from = moment(startDay, 'DD-MM-YYYY hh:mm:ss');
     const diff = moment().diff(from);
     const elapsedDuration = moment.duration(diff);
-    var percent = (elapsedDuration.asMinutes() / (60 * 4)) * 100;
+    var percent = (elapsedDuration.asMinutes() / (60 * hours)) * 100;
 
-
-    var s = Math.floor(elapsedDuration.asHours()) + moment(diff).format(":mm:ss");
-    console.log({
-        from,
-        diff,
-        elapsedDuration: s,
-        percent
-    });
+    //var s = Math.floor(elapsedDuration.asHours()) + moment(diff).format(":mm:ss");
+    // console.log({
+    //     from,
+    //     diff,
+    //     elapsedDuration: s,
+    //     percent
+    // });
     return Math.floor(parseInt(percent));
 }
 
