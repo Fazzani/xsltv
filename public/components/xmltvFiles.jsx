d7d1b5dd7265ddc5aec55c5559d8689915b7c1f2
Fazzani <tunisienheni@outlook.com>
2018-10-15 18:01:55 +0200

    vscode push

--- public/components/xmltvFiles.jsx
+++ public/components/xmltvFiles.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import '../styles/file-items.scss';
-import { Constants } from '../js/common';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import '../styles/file-items.scss'
+import { Constants } from '../js/common'
 
 export default class XmltvFilesComponent extends Component {
   static propTypes = {
@@ -9,20 +9,29 @@ export default class XmltvFilesComponent extends Component {
       PropTypes.shape({
         name: PropTypes.string.isRequired,
         url: PropTypes.string.isRequired,
-      }),
+      })
     ),
     onSettingsModalCallback: PropTypes.func,
-  };
+  }
 
   render() {
     return (
       <div className="xviewer-list">
         {this.props.files.map((file, key) => {
-          return <XmltvFileItem file={file} key={key} itemCallback={this.props.onSettingsModalCallback} />;
+          return (
+            <XmltvFileItem
+              file={file}
+              key={key}
+              itemCallback={this.props.onSettingsModalCallback}
+            />
+          )
         })}
-        <XmltvFileItem isNew={true} itemCallback={this.props.onSettingsModalCallback} />
+        <XmltvFileItem
+          isNew={true}
+          itemCallback={this.props.onSettingsModalCallback}
+        />
       </div>
-    );
+    )
   }
 }
 
@@ -34,50 +43,58 @@ class XmltvFileItem extends Component {
     }),
     itemCallback: PropTypes.func,
     isNew: PropTypes.bool,
-  };
+  }
 
   static defaultPropTypes = {
     isNew: false,
-  };
+  }
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       isNew: props.isNew,
-    };
+    }
   }
-  view = e => {
-    e.preventDefault();
+
+  componentDidMount() {
+    this.setState({
+      isNew: !this.props.file ? true : this.props.isNew,
+    })
+  }
+
+  view = (e) => {
+    e.preventDefault()
     if (this.props.itemCallback) {
       this.props.itemCallback({
         type: Constants.Events.LOAD_XMLTV_URL,
         file: this.createFileObject(this.props.file.url),
-      });
+      })
     }
-  };
+  }
 
-  add = e => {
-    e.preventDefault();
+  add = (e) => {
+    e.preventDefault()
     if (this.props.itemCallback) {
       this.props.itemCallback({
         type: Constants.Events.ADD_XMLTV_URL,
         file: this.createFileObject(this.state.value),
-      });
+      })
     }
-  };
+    this.setState({ value: '' })
+  }
 
-  remove = e => {
-    e.preventDefault();
+  remove = (e) => {
+    e.preventDefault()
     if (this.props.itemCallback) {
       this.props.itemCallback({
         type: Constants.Events.REMOVE_XMLTV_URL,
         file: this.createFileObject(this.props.file.url),
-      });
+      })
     }
-  };
+  }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    this.setState({ value: event.target.value })
   }
 
   /**
@@ -85,38 +102,51 @@ class XmltvFileItem extends Component {
    * @param {string} xmltv_file_url - xmltv file url
    * @memberof SettingsModal
    */
-  createFileObject = xmltv_file_url => {
-    return { name: xmltv_file_url.split('/').pop(), url: xmltv_file_url };
-  };
+  createFileObject = (xmltv_file_url) => {
+    return { name: xmltv_file_url.split('/').pop(), url: xmltv_file_url }
+  }
 
   render() {
-    this.state.isNew = !this.props.file ? true : this.props.isNew;
-
     return (
       <div className="row xviewer-list-item">
         {this.state.isNew ? (
-          <div className="col">
-            <input type="url" value={this.state.value || ''} onChange={e => this.handleChange(e)} placeholder="Add new xmltv url..." className="form-control" />
+          <div className="col-8">
+            <input
+              type="url"
+              value={this.state.value || ''}
+              onChange={(e) => this.handleChange(e)}
+              placeholder="Add new xmltv url..."
+              className="form-control"
+            />
           </div>
         ) : (
           <div className="col">{this.props.file.name}</div>
         )}
         <div className="col">
-          <button className="pull-right btn btn-sm border-0 rounded-circle btn-outline-success" onClick={e => this.view(e)}>
+          <button
+            className="pull-right btn btn-sm border-0 rounded-circle btn-outline-success"
+            onClick={(e) => this.view(e)}
+          >
             <i className="fa fa-eye" />
           </button>
           {this.state.isNew && (
-            <button className="pull-right btn btn-sm border-0 rounded-circle btn-outline-primary" onClick={e => this.add(e)}>
+            <button
+              className="pull-right btn btn-sm border-0 rounded-circle btn-outline-primary"
+              onClick={(e) => this.add(e)}
+            >
               <i className="fa fa-plus" />
             </button>
           )}
           {!this.state.isNew && (
-            <button className="pull-right btn btn-sm border-0 rounded-circle btn-outline-danger" onClick={e => this.remove(e)}>
+            <button
+              className="pull-right btn btn-sm border-0 rounded-circle btn-outline-danger"
+              onClick={(e) => this.remove(e)}
+            >
               <i className="fa fa-trash-o" />
             </button>
           )}
         </div>
       </div>
-    );
+    )
   }
 }
