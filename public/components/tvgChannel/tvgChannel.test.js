1caa7ef49594449a1bc4f0839691b7f3caa227e4
Fazzani <tunisienheni@outlook.com>
2018-10-24 17:05:53 +0200

    vscode deploy

---
+++ public/components/tvgChannel/tvgChannel.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import TvgChannel from './tvgChannel'
+
+describe('<TvgChannel/>', () => {
+  it('matches the snapshot ', () => {
+    const tree = shallow(<TvgChannel />)
+    expect(tree).toMatchSnapshot()
+  })
+})
