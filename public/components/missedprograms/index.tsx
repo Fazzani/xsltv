afd7c60f4bba9c7f373c556bb928a756d98b2b25
Heni Fazzani <heni.fazzani@seloger.com>
2019-02-09 12:51:42 +0100

    fix no-verify

--- public/components/missedprograms/index.tsx
+++ public/components/missedprograms/index.tsx
@@ -1,32 +1,46 @@
-import * as React from "react";
-import elasticServices from "../../services/elasticService";
-import "./style.scss";
+import * as React from 'react'
+import elasticServices from '../../services/elasticService'
+import './style.scss'
 
 export class MissedProgramsState {
-  value: string = "";
-  query?: object;
-  result?: any;
-  status: number = -1;
-  size: number = 1000;
+  value: string = ''
+  query?: object
+  result?: any
+  groups: any[string]
+  status: number = -1
+  size: number = 1000
+  groupFilter: string = 'all'
 }
 
 export interface IQuery {
-  field: string;
-  value: string;
+  field: string
+  value: string
 }
 
-export default class MissedPrograms extends React.Component<
-  {},
-  MissedProgramsState
-> {
+export default class MissedPrograms extends React.Component<{}, MissedProgramsState> {
   constructor(props: any) {
-    super(props);
-    this.state = { value: "", status: -1, size: 1000 };
+    super(props)
+    this.state = { value: '', status: -1, size: 1000, groups: [], groupFilter: 'all' }
   }
 
   async componentDidMount() {
-    var allChannels = await elasticServices.searchAsync(this.queryFactory());
-    this.setState({ result: allChannels });
+    var allChannels = await elasticServices.searchAsync(this.queryFactory())
+    this.setState({ result: allChannels.inner_hits.channels_without_epg.hits.hits }, () => {
+      this.setGroups()
+    })
+  }
+
+  setGroups = () => {
+    const groups: any[string] = this.state.result.reduce(
+      (groups: any[string], item: any) => ({
+        ...groups,
+        [item._source.url]: [...(groups[item._source.url] || []), item],
+      }),
+      {}
+    )
+
+    groups['all'] = this.state.result
+    this.setState({ groups })
   }
 
   queryFactory = () => {
@@ -36,103 +50,91 @@ export default class MissedPrograms extends React.Component<
       _source: false,
       query: {
         nested: {
-          path: "tv.channel",
-          score_mode: "avg",
+          path: 'tv.channel',
+          score_mode: 'avg',
           query: {
             bool: {
-              should: []
-            }
+              should: [],
+            },
           },
           inner_hits: {
-            name: "channels_without_epg",
-            size: this.state.size
-          }
-        }
-      }
-    };
+            name: 'channels_without_epg',
+            size: this.state.size,
+          },
+        },
+      },
+    }
     if (this.state.status !== undefined && this.state.status !== -1) {
       queryBase.query.nested.query.bool.should.push({
         match: {
-          "tv.channel.active": this.state.status === 1
-        }
-      });
+          'tv.channel.active': this.state.status === 1,
+        },
+      })
     }
-    if (this.state.value && this.state.value !== "") {
+    if (this.state.value && this.state.value !== '') {
       // console.log(`${this.state.value}`)
       queryBase.query.nested.query.bool.should.push({
         wildcard: {
-          "tv.channel.id": { value: "*" + this.state.value + "*", boost: 2.0 }
-        }
-      });
+          'tv.channel.id': { value: '*' + this.state.value + '*', boost: 2.0 },
+        },
+      })
       queryBase.query.nested.query.bool.should.push({
         wildcard: {
-          "tv.channel.url": "*" + this.state.value + "*"
-        }
-      });
+          'tv.channel.url': '*' + this.state.value + '*',
+        },
+      })
     }
-    return queryBase;
-  };
+    return queryBase
+  }
 
   handleChange(e: React.FormEvent<HTMLInputElement>): void {
-    e.preventDefault();
+    e.preventDefault()
     this.setState({ value: (e.target as HTMLInputElement).value }, async () => {
-      const result = await elasticServices.searchAsync(this.queryFactory());
-      // console.log(`result => ${result}`)
-      this.setState({ result });
-    });
+      const result = await elasticServices.searchAsync(this.queryFactory())
+      this.setState({ result: result.inner_hits.channels_without_epg.hits.hits }, () => {
+        this.setGroups()
+      })
+    })
   }
 
   handleChangeStatus(e: React.ChangeEvent<HTMLSelectElement>): void {
-    e.preventDefault();
-    console.log(e.target.value);
+    e.preventDefault()
     this.setState({ status: Number.parseInt(e.target.value) }, async () => {
-      const result = await elasticServices.searchAsync(this.queryFactory());
-      console.log(`result => ${result}`);
-      this.setState({ result });
-    });
+      const result = await elasticServices.searchAsync(this.queryFactory())
+      this.setState({ result: result.inner_hits.channels_without_epg.hits.hits }, () => {
+        this.setGroups()
+      })
+    })
+  }
+
+  handleChangeGroups(e: React.ChangeEvent<HTMLSelectElement>): void {
+    e.preventDefault()
+    this.setState({ groupFilter: e.target.value, result: this.state.groups[e.target.value] })
   }
 
   render() {
     const ChannelIcon = ({ ch }) => {
-      return ch._source.icon ? (
-        <img
-          className="card-img-top"
-          src={ch._source.icon.src}
-          alt="Card image cap"
-          style={{ width: "5rem" }}
-        />
-      ) : null;
-    };
+      return ch._source.icon ? <img className="card-img-top" src={ch._source.icon.src} alt="Card image cap" style={{ width: '5rem' }} /> : null
+    }
     const ListChannels = (props: any) => {
-      return props &&
-        props.value &&
-        props.value.inner_hits.channels_without_epg ? (
-        props.value.inner_hits.channels_without_epg.hits.hits.map(
-          (channel: any, i: number) => (
-            <div
-              className={
-                "card text-white bg-primary mb-3 " +
-                (channel._source.active ? "enabled" : "disabled")
-              }
-              key={channel._source.id + i}
-              style={{ width: "18rem" }}
-            >
-              <ChannelIcon ch={channel} />
-              <div className="card-body">
-                <h6 className="card-subtitle mb-2 text-muted">
-                  {channel._source.id}
-                </h6>
-                <h5 className="card-title">{channel._source.country}</h5>
-                <p>{channel._source.url}</p>
-                <p>{channel._source.active}</p>
-              </div>
+      return props && props.value ? (
+        props.value.map((channel: any, i: number) => (
+          <div
+            className={'card text-white bg-primary mb-3 ' + (channel._source.active ? 'enabled' : 'disabled')}
+            key={channel._source.id + i}
+            style={{ width: '18rem' }}>
+            <ChannelIcon ch={channel} />
+            <div className="card-body">
+              <h5 className="card-title">{channel._source.id}</h5>
+              <h6 className="card-subtitle mb-2 text-muted">{channel._source.country}</h6>
+              <p>{channel._source.url}</p>
             </div>
-          )
-        )
+          </div>
+        ))
       ) : (
         <div>No Result</div>
-      );
-    };
+      )
+    }
 
     return (
       <div className="container">
@@ -144,17 +146,23 @@ export default class MissedPrograms extends React.Component<
             value={this.state.value}
             autoFocus
             onChange={e => {
-              this.handleChange(e);
+              this.handleChange(e)
             }}
           />
         </div>
         <div className="form-group">
+          <label htmlFor="statusChannelList">Channels groups</label>
+          <select className="form-control" id="groupsChannelList" onChange={e => this.handleChangeGroups(e)}>
+            {Object.keys(this.state.groups).map((g, i) => (
+              <option value={g} selected={this.state.groupFilter === g}>
+                {g}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="form-group">
           <label htmlFor="statusChannelList">Channel status</label>
-          <select
-            className="form-control"
-            id="statusChannelList"
-            onChange={e => this.handleChangeStatus(e)}
-          >
+          <select className="form-control" id="statusChannelList" onChange={e => this.handleChangeStatus(e)}>
             <option value="-1">All</option>
             <option value="0">disabled</option>
             <option value="1">enabled</option>
@@ -164,6 +172,6 @@ export default class MissedPrograms extends React.Component<
           <ListChannels value={this.state.result} />
         </div>
       </div>
-    );
+    )
   }
 }
